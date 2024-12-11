065422ac258855a8c1fe82b91a934721d3d0551a
Andrhan04 <andrham2004@yandex.ru>
2024-12-11 11:32:11 +0800

    modified:   postInstall.sh

--- postInstall.sh
+++ postInstall.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 DIR=\\PortableGit\\_home
+REP=$DIR\\_repos
+SSH_K=$DIR\\ssh_keys
 AGRE=y
 if [ -d "$DIR" ]; then
-    read -p "Ситраю нахер?[y/n]: " par
+    read -p "Есть $DIR . Перезаписать нахер?[y/n]: " par
     if [[ "$par" =~ $AGRE ]]; then
     rm -r \\PortableGit\\_home 
-    mkdir \\PortableGit\\_home \\PortableGit\\_home\\_repos \\PortableGit\\_home\\ssh_keys
-    else echo "$DIR does exist."
+    mkdir $DIR $REP $SSH_K
+    echo $DIR " Было перезприсан"
+    echo $REP " Было перезприсан"
+    echo $SSH_K " Было перезприсан"
+    else echo "$DIR существует"
     fi
 else
-    mkdir \\PortableGit\\_home \\PortableGit\\_home\\_repos \\PortableGit\\_home\\ssh_key
+    mkdir $DIR $REP $SSH_K
+    echo $DIR " Было создано"
+    echo $REP " Было создано"
+    echo $SSH_K " Было создано"
 fi
 
-file_a=_user_settings.sh
-file_b=PortableGit\\etc\\profile.d\\_user_settings.sh
+file_Myconf=_user_settings.sh
+file_In_Directory=PortableGit\\etc\\profile.d\\
 
-if [ -f $file_b ];then
-    if cmp -s $file_a $file_b ; 
-    then echo Same 
+if [ -f $file_In_Directory$file_Myconf ];then
+    if cmp -s $file_Myconf  $file_In_Directory$file_Myconf ; 
+    then echo "Файлы $file_Myconf и $file_In_Directory$file_Myconf одинаковы" 
     else 
-        read -p "Ситраю нахер? [y/n]: " par
+        read -p "Перезаписать файл? $file_In_Directory$file_Myconf [y/n]: " par
         if [[ "$par" =~ $AGRE ]]; then
             rm -r \\PortableGit\\etc\\profile.d\\_user_settings.sh
             cp _user_settings.sh  \\PortableGit\\etc\\profile.d\\_user_settings.sh 
-
-        else echo "Ну и иди нафиг"
+            echo "Файл $file_In_Directory$file_Myconf был перезаписан"
+        else echo $file_In_Directory$file_Myconf " был не тронут"
         fi
     fi
 else
-    cp _user_settings.sh \\PortableGit\\etc\\profile.d\\
+    cp $file_Myconf $file_In_Directory
+    echo "Скопирован файл"
 fi
